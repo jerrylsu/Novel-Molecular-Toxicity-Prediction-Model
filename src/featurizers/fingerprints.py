119911ba7c652bdb320fa70ccc36c1a0ea4ec19b
Jerry <sa517301@mail.ustc.edu.cn>
2021-02-02 09:20:20 +0000

    state

--- src/featurizers/fingerprints.py
+++ src/featurizers/fingerprints.py
@@ -1,5 +1,6 @@
 from typing import Optional, List, Tuple
 import os
+import json
 import sys
 from argparse import ArgumentParser
 from tqdm import tqdm
@@ -20,7 +21,14 @@ class FingerPrints(object):
     def __init__(self, args):
         self.args = args
         self.vocab_freq = {}
+        self.molecule_names = []
         self.error_num = 0
+        if not os.path.exists(self.args.labels_vocab):
+            print(f"The labels vocab is not exist, and create it ...")
+            self.labels_dict = self._create_labels_dictinary(self.args.labels_file)
+        else:
+            print(f"Load labels vocab from labels_vocab.pt cache.")
+            self.labels_dict = torch.load(self.args.labels_vocab)
         if not os.path.exists(self.args.smiles_vocab):
             print(f"The smiles vocab is not exist, and create it ...")
             self.vocab_freq = self._update_vocab_frequency(self.args.smiles_file,
@@ -52,11 +60,13 @@ class FingerPrints(object):
                 if not molecule:
                     break
                 line_num += 1
-                molecule = molecule.split(sep)[0].strip() + f" {str(line_num)}"
+                molecule_name, molecule_id = molecule.strip().split(sep)
+                molecule = molecule_name + f" {str(line_num)}"
 
                 # molecule: <csfpy.Molecule named '0' (id 4294967295, 34 atoms) [0x0000013cd4fa55c0]>
                 try:
                     molecule = csfpy.Molecule(molecule)
+                    self.molecule_names.append(molecule_name)
                 except RuntimeError as re:
                     self.error_num += 1
                     continue
@@ -68,11 +78,17 @@ class FingerPrints(object):
                 # SparsIntVec objects can be converted to lists of integers
                 fingerprint_list = fingerprint.toList()
 
-                yield fingerprint_list
+                yield molecule_name, self.labels_dict[molecule_id], fingerprint_list
+
+    def _molecule_to_list(self, molecule: str):
+        molecule = csfpy.Molecule(molecule)
+        fingerprint = csfpy.csfp(molecule, 2, 5)
+        fingerprint_list = fingerprint.toList()
+        return fingerprint_list
 
     def _update_vocab_frequency(self, smiles_file, smiles_vocab, sep=","):
         fingerprints_generator = self._fingerprints_generator(smiles_file, sep=sep)
-        for fp_list in tqdm(fingerprints_generator, desc="FingerPrint List"):
+        for _, _, fp_list in tqdm(fingerprints_generator, desc="FingerPrint List"):
             for elem in fp_list:
                 self.vocab_freq[elem] = self.vocab_freq.get(elem, 0) + 1
         vocab_freq_ordered = sorted(self.vocab_freq.items(), key=lambda x: x[1], reverse=True)
@@ -86,6 +102,28 @@ class FingerPrints(object):
     def _create_dictionary(self, vocab_freq_squeezed):
         return {data[0]: index for index, data in enumerate(vocab_freq_squeezed)}
 
+    def _create_labels_dictinary(self, labels_file):
+        labels_dict = {}
+        with open(labels_file, 'r') as fp:
+            lines = fp.readlines()
+            for line in lines:
+                molecule_id, molecule_toxicity = line.strip().split(",")
+                labels_dict[molecule_id] = 0 if molecule_toxicity == "N" else 1
+        torch.save(labels_dict, self.args.labels_vocab)
+        return labels_dict
+
+    def _show_duplicate_data(self, molecule_names, labels, one_hots):
+        print(f"Total size: {len(one_hots)}")
+        print(f"Total size that remove duplicate:"
+              f"{len(list(map(lambda x: list(x), list(set(list(map(lambda x: tuple(x), one_hots)))))))}")
+        one_hots = [tuple(one_hot) for one_hot in one_hots]
+        dic = {}
+        for molecule_name, label, onehot in zip(molecule_names, labels, one_hots):
+            dic[onehot] = dic.get(onehot, []) + [molecule_name]
+        duplicate_data = list(filter(lambda x: len(x[1]) > 1, dic.items()))
+        for data in duplicate_data:
+            print(data[1])
+
     def _to_onehot(self, fp_list: Optional[List[int]]) -> Tuple[int, int, List[int]]:
         one_hot = [0] * len(self.dict)
         miss, total = 0, len(fp_list)
@@ -97,31 +135,34 @@ class FingerPrints(object):
         return miss, total, one_hot
 
     def to_onehot(self):
-        miss_all, total_all, one_hots = 0, 0, []
-        with open("../../data/jerry_train.csv") as fp:
-            lines = fp.readlines()
-            for line in lines:
-                line = list(filter(lambda x: x != '', line.split(",")))[2:-1]
-                line = list(map(lambda x: int(x), line))
-                miss, total, one_hot = self._to_onehot(line)
-                miss_all = miss_all + miss
-                total_all = total_all + total
-        #for fp_list in tqdm(self._fingerprints_generator(self.args.smiles_file), desc="Convert to onehot:"):
-        #    one_hots.append(self._to_onehot(fp_list))
-        res = miss_all / total_all
-        res = list(map(lambda x: sum(x), one_hots))
-        # 删除全0
+        miss_all, total_all, molecule_names, labels, one_hots = 0, 0, [], [], []
+        for molecule_name, label, fp_list in tqdm(self._fingerprints_generator(self.args.smiles_file, sep="\t"), desc="Convert to onehot:"):
+            miss, total, onehot = self._to_onehot(fp_list)
+            molecule_names.append(molecule_name)
+            labels.append(label)
+            one_hots.append(onehot)
+            miss_all = miss_all + miss
+            total_all = total_all + total
+        print(f"Miss rate: {round(miss_all / total_all, 4) * 100}%")
 
-        # 删除重复
+        aaa = self._molecule_to_list('[2H]C(=O)N(C([2H])([2H])[2H])C([2H])([2H])[2H]')
+        print(aaa)
+        bbb = self._molecule_to_list('CN(C)C=O')
+        print(bbb)
+        print(aaa==bbb)
+        raise  "Jerry"
+        self._show_duplicate_data(molecule_names, labels, one_hots)
 
         return one_hots
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--smiles_file", type=str, default="../../data/test.csv", help="Path of the smiles file.")
+    parser.add_argument("--smiles_file", type=str, default="../../data/Jiang1823Train.smi", help="Path of the smiles file.")
+    parser.add_argument("--labels_file", type=str, default="../../data/LabelTrainValidate.csv", help="Path of the labels file.")
     parser.add_argument("--update_smiles_file", type=str, default=None, help="Path of the smiles file.")
-    parser.add_argument("--smiles_vocab", type=str, default="../../data/vocab/smiles_vocab_190w.pt", help="Path of the smiles vocab file.")
+    parser.add_argument("--smiles_vocab", type=str, default="../../data/vocab/update_smiles_vocab.pt", help="Path of the smiles vocab file.")
+    parser.add_argument("--labels_vocab", type=str, default="../../data/vocab/labels_vocab.pt", help="Path of the labels vocab file.")
     parser.add_argument("--update_smiles_vocab", type=str, default="../../data/vocab/update_smiles_vocab.pt", help="Path of the smiles vocab file.")
     parser.add_argument("--upper", type=int, default=2000000, help="the upper of squeeze vocab.")
     parser.add_argument("--lower", type=int, default=0, help="the lower of squeeze vocab.")
