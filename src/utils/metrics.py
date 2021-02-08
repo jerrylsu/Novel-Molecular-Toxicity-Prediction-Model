b4ebfcee116dc622e6e5bdefa00e2975767b80bb
Jerry <sa517301@mail.ustc.edu.cn>
2021-02-08 09:30:25 +0000

    add metrics

---
+++ src/utils/metrics.py
@@ -0,0 +1,23 @@
+from sklearn import metrics
+from typing import List
+
+
+class Metrics(object):
+    def __init__(self):
+        pass
+
+    def calculate_accuracy(self, y_true: List, y_pred: List, normalize: bool=True):
+        return metrics.accuracy_score(y_true, y_pred, normalize)
+
+    def calculate_auc(self, y_true: List, y_pred: List):
+        fpr, tpr, thresholds = metrics.roc_curve(y_true, y_pred)
+        return metrics.auc(fpr, tpr)
+
+    def calculate_precision(self, y_true: List, y_pred: List):
+        return metrics.precision_score(y_true, y_pred)
+
+    def calculate_recall(self, y_true: List, y_pred: List):
+        return metrics.recall_score(y_true, y_pred)
+
+    def calculate_f1(self, y_true: List, y_pred: List):
+        return metrics.f1_score(y_true, y_pred)
