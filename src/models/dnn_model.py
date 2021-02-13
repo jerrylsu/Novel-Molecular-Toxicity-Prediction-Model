6a0bb7aed0affa49385606928b72e11f3b5aed15
jerry <sa517301@mail.ustc.edu.cn>
2021-02-13 14:53:25 +0800

    dnn model

--- src/models/dnn_model.py
+++ src/models/dnn_model.py
@@ -8,13 +8,7 @@ class DNNModel(nn.Module):
 
         # DNN model
         self.dnn_model = nn.Sequential(
-            nn.Linear(input_size, 2048),
-            nn.Dropout(0.2),
-            nn.LeakyReLU(),
-            nn.Linear(2048, 1024),
-            nn.Dropout(0.2),
-            nn.LeakyReLU(),
-            nn.Linear(1024, 512),
+            nn.Linear(input_size, 512),
             nn.Dropout(0.2),
             nn.LeakyReLU(),
             nn.Linear(512, 256),
