9f8c54e60c28a2224983a571f839f27adeb78b2a
jerry <sa517301@mail.ustc.edu.cn>
2021-02-27 20:30:31 +0800

    debug

--- src/trainer.py
+++ src/trainer.py
@@ -49,7 +49,7 @@ class Trainer:
             self.classifier_model = CapsuleModel(conv_inputs=1,
                                                  conv_outputs=1,      # 256,
                                                  num_primary_units=8,
-                                                 primary_unit_size=8*253,  # fixme get from conv2d
+                                                 primary_unit_size=8*509,  # fixme get from conv2d  61(128)---253(512)--509(1024)
                                                  num_output_units=2,           # one for each MNIST digit
                                                  output_unit_size=16).to(self.args.device)
         else:
